c325a80e551abacfd97494c95219664994bad117
paritosh <paritoshpurohit29@gmail.com>
2025-04-01 15:45:57 +0530

    questions

--- que9.cpp
+++ que9.cpp
@@ -1,15 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-
-
-
-
-
+bool onlydigit(string ch){
+    for(int i=0;i<=ch.length();i++){
+      if(ch[i]<'0' || ch[i]>'9'){   //1-47 ....57-128  0=48 and 9=57
+         return true;
+       }
+    }
+    return false;
+    
+}
 int main()
 {
-    string s;
-    cout<<"enter the string";
-    cin>>s;
+    string ch="123";
+    // cout<<"enter the string ";
+    // cin>>ch;
+    bool digitornot=onlydigit(ch);
+    if(digitornot){
+        cout<<"it aplhanumeric";
+    }
+    else{
+        cout<<"it contain only digit";
+    }
     return 0;
 }
